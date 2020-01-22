fa86bed860e7d78991362558702e7c01ade762dc
malsaadan <mmalsaadan@gmail.com>
2020-01-22 13:19:49 +0300

    Lesson solutions

---
+++ solution.js
@@ -0,0 +1,8 @@
+const getLastElement = function(array){
+    return array[array.length - 1];
+}
+
+const nums = [10, 9, 6, 5];
+const strs = ['t', 'm', 'u', 'z', 6, 'i'];
+const strs2 = [['t','m','u'],['z',6,'i']]
+const strs3 = [['t'],'m','u','z',6,['i']] 
